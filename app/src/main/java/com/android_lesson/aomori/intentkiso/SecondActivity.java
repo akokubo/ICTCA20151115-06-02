7e237d811f74becf6989e14c35f73404ee80f26e
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-11-17 14:32:41 +0900

    MainActivityからSecondActivityにevent_dateとevent_nameを渡す

--- app/src/main/java/com/android_lesson/aomori/intentkiso/SecondActivity.java
+++ app/src/main/java/com/android_lesson/aomori/intentkiso/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.android_lesson.aomori.intentkiso;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String event_date = intent.getStringExtra("event_date");
+        String event_name = intent.getStringExtra("event_name");
+
+        Toast.makeText(getApplicationContext(), event_date + ":" + event_name, Toast.LENGTH_SHORT).show();
     }
 }
